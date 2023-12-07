0e7a4e865e985a645d7608625239e7b96b6cbe22
Mavester432 <mavester432@gmail.com>
2023-12-07 09:57:53 -0800

    get_env.c

--- get_env.c
+++ get_env.c
@@ -1,26 +1,24 @@
 #include "shell.h"
-#include <stdlib.h>
 
 /**
  * _getenv - Get the value of an environment variable
- * @name: Name of the enviroment variable
+ * @envp: Array of environment variables
+ * @name: Name of the environment variable
  *
  * Return: Value of the environment variable, or NULL if it doesn't exist
  */
 
-char *_getenv(const char *name)
+char *_getenv(char **envp, const char *name)
 {
-	char **env;
 	size_t name_len = strlen(name);
 
-	for (env = environ; *env != NULL; env++)
+	for (; *envp != NULL; envp++)
 	{
-	if (strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
+	if (strncmp(*envp, name, name_len) == 0 && (*envp)[name_len] == '=')
 	{
-	return (*env + name_len + 1);
+	return (*envp + name_len + 1);
 	}
 	}
 
-	return (NULL);
-
+return (NULL);
 }

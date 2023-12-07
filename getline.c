4dec2d908605e49f606c86d10c29f77867a77ddc
Mavester432 <mavester432@gmail.com>
2023-12-07 04:22:16 -0800

    getline.c

---
+++ getline.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * get_line - Read input from the standard input.
+ *
+ * Return: The string entered by the user.
+ */
+
+char *get_line(void)
+{
+	char *buffer = NULL;
+	size_t buf_size = 0;
+	ssize_t buf_read;
+
+	buf_read = getline(&buffer, &buf_size, stdin);
+
+	if (buf_read == -1)
+	{
+	perror("getline");
+	free(buffer);
+	return (NULL);
+	}
+
+	if (buf_read > 0 && buffer[buf_read - 1] == '\n')
+	{
+		buffer[buf_read - 1] = '\0';
+	}
+
+	return (buffer);
+}

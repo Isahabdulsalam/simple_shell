2be038d74fa97bee5a32b0670914287f79459aff
Mavester432 <mavester432@gmail.com>
2023-12-07 07:15:08 -0800

    shell.h

---
+++ shell.h
@@ -0,0 +1,48 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define PATH_MAX_LENGTH 4096
+#define PATH_SEPARATOR ":"
+#define PROMPT "$ "
+#define MAX_TOKENS 1024
+
+/* built-in funcs */
+int check_for_builtin(char **args)
+int shell_env(void)
+void shell_exit(char **args)
+void shell_help(void)
+
+/* get_line.c*/
+void *get_line(void);
+
+/* get_path.c */
+char *get_path(void);
+char *find_in_path(char *command)
+
+/* get_env.c */
+char *_getenv(const char *name);
+
+/* error.c */
+void _eputs(char *str)
+int _eputchar(char c)
+int _putfd(char c, int fd)
+int _putsfd(char *str, int fd)
+
+#endif

60864d5ddb66fec9b6180cababd89261f1c42d9d
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-12-13 01:32:57 +0100

    team project

---
+++ builtin_replacememt.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
+#define MAX_ALIASES 50
+#define MAX_ALIAS_LENGTH 50
+
+void replace_variables(char *input) {
+    char pid[20];
+    int last_command = 0;
+    char exit_status[20];
+    char *pos = strstr(input, "$$");
+
+    sprintf(pid, "%d", getpid());
+
+    if (pos != NULL) {
+        memmove(pos + strlen(pid), pos + 2, strlen(pos + 2) + 1);
+        memcpy(pos, pid, strlen(pid));
+    }
+
+    sprintf(exit_status, "%d", last_command);
+
+    pos = strstr(input, "$?");
+    if (pos != NULL) {
+        memmove(pos + strlen(exit_status), pos + 2, strlen(pos + 2) + 1);
+        memcpy(pos, exit_status, strlen(exit_status));
+    }
+}
+

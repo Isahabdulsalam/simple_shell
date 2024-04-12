979189746a7cc9e78d4230c9f19dd34cfbc0765f
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-04-12 13:21:52 +0100

    shell

--- input.c
+++ input.c
@@ -1,41 +1,94 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-static char *last_input;
 
 /**
- * get_input - Read a line of input from the user.
- *
- * Return: Pointer to a buffer containing the user's input.
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
  */
-char *get_input(void)
+
+char **strtow(char *str, char *d)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
-    ssize_t characters;
+	int i, j, k, m, numwords = 0;
+	char **s;
 
-    
-    printf("Shell $ ");
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+			numwords++;
 
-    
-    characters = getline(&buffer, &bufsize, stdin);
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (is_delim(str[i], d))
+			i++;
+		k = 0;
+		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
 
-    
-    if (characters == -1)
-    {
-        perror("getline");
-        exit(EXIT_FAILURE);
-    }
+/**
+ * **strtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+char **strtow2(char *str, char d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
 
-   
-    if (buffer[characters - 1] == '\n')
-    {
-        buffer[characters - 1] = '\0';
-    }
-
-   
-    last_input = buffer;
-
-    return (buffer);
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
 }
--- memory.c
+++ memory.c
@@ -1,63 +1,18 @@
 #include "shell.h"
-#include <unistd.h>
-
-extern char **environ;
 
 /**
- * free_error - frees alloc'd pointers following system error
- * @argv: pointer to a pointer to an array of pointers
- * @arg: pointer to a pointer to an array of characters
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
  *
- * Return: void.
+ * Return: 1 if freed, otherwise 0.
  */
-void free_error(char **argv, char *arg)
+int bfree(void **ptr)
 {
-	int i;
-
-	for (i = 0; argv[i]; i++)
-		free(argv[i]);
-	free(argv);
-	free(arg);
-	exit(EXIT_FAILURE);
-}
-
-/**
- * free_tokens - frees memory allocated dynamically by tokenize()
- * @ptr: pointer to allocated memory
- *
- * Return: void.
- */
-void free_tokens(char **ptr)
-{
-	int i;
-
-	for (i = 0; ptr[i]; i++)
-		free((ptr[i]));
-	free(ptr);
-}
-
-
-/**
- * free_path - Frees the global variable containing the PATH environment
- *              variable value
- *
- * Return: Nothing
- */
-void free_path(void)
-{
-	if (environ != NULL)
+	if (ptr && *ptr)
 	{
-		size_t i = 0;
-
-		while (environ[i] != NULL)
-		{
-			if (strncmp(environ[i], "PATH=", 5) == 0)
-			{
-				free(environ[i]);
-				environ[i] = NULL;
-				break;
-			}
-			i++;
-		}
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
 	}
+	return (0);
 }

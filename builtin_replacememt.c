15c2376a7b4c1d6312555d9a2ff7c43fc9b4bdfe
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-04-12 13:43:11 +0100

    simple_shell

--- builtin_replacememt.c
+++ builtin_replacememt.c
@@ -6,25 +6,25 @@
 #define MAX_ALIASES 50
 #define MAX_ALIAS_LENGTH 50
 
-void replace_variables(char *input) {
-    char pid[20];
-    int last_command = 0;
-    char exit_status[20];
-    char *pos = strstr(input, "$$");
+void replace_variables(char *input)
+{
+	char pid[20];
+	int last_command = 0;
+	char exit_status[20];
+	char *pos = strstr(input, "$$");
+	sprintf(pid, "%d", getpid());
 
-    sprintf(pid, "%d", getpid());
+	if (pos != NULL) 
+	{
+		memmove(pos + strlen(pid), pos + 2, strlen(pos + 2) + 1);
+		memcpy(pos, pid, strlen(pid));
+	}
+	sprintf(exit_status, "%d", last_command);
+	pos = strstr(input, "$?");
 
-    if (pos != NULL) {
-        memmove(pos + strlen(pid), pos + 2, strlen(pos + 2) + 1);
-        memcpy(pos, pid, strlen(pid));
-    }
-
-    sprintf(exit_status, "%d", last_command);
-
-    pos = strstr(input, "$?");
-    if (pos != NULL) {
-        memmove(pos + strlen(exit_status), pos + 2, strlen(pos + 2) + 1);
-        memcpy(pos, exit_status, strlen(exit_status));
-    }
+	if (pos != NULL) {
+		memmove(pos + strlen(exit_status), pos + 2, strlen(pos + 2) + 1);
+		memcpy(pos, exit_status, strlen(exit_status));
+	}
 }
 

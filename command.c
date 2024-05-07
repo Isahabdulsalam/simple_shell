13be8e05c2ca3bc5bca4d44fef8d87c41c0e6f42
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-05-07 22:46:29 +0100

    done

--- command.c
+++ command.c
@@ -30,27 +30,3 @@ int main(void) {
     }
     return 0;
 }
-
-void print_prompt(void) {
-    printf("simple_shell$ ");
-}
-
-int execute_command(char *line) {
-    pid_t pid;
-    int status;
-    
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        return -1;
-    } else if (pid == 0) {
-        if (execlp(line, line, NULL) == -1) {
-            perror("execlp");
-            return -1;
-        }
-    } else {
-        waitpid(pid, &status, 0);
-    }
-    return 0;
-}
-

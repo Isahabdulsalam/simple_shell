b7272dd88357e412cd4e583d0c68c482a0e19e11
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-05-07 02:22:32 +0100

    done

---
+++ command.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+int main(void) {
+  char line[MAX_LINE_LENGTH];
+  while (1) {
+    print_prompt();
+    if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL) {
+      printf("\n");
+      break;
+    }
+    line[strcspn(line, "\n")] = '\0';
+    if (strcmp(line, "exit") == 0) {
+      break;
+    } else if (strcmp(line, "env") == 0) {
+      extern char *environ;
+      for (char *env = environ; *env != NULL; env++) {
+        printf("%s\n", *env);
+      }
+    } else {
+      if (execute_command(line) == -1) {
+        printf("%s: command not found\n", line);
+      }
+    }
+  }
+  return 0;
+}
+
+void print_prompt(void) {
+  printf("#cisfun$ ");
+}
+
+int execute_command(char *line) {
+  return execlp(line, line, NULL);
+}

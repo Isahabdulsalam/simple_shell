378cfd5b820f8127f185e332a59d857f288b4915
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-05-07 02:42:03 +0100

    shell

--- command.c
+++ command.c
@@ -5,20 +5,20 @@ int main(void) {
   while (1) {
     print_prompt();
     if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL) {
-      printf("\n");
+      printf("n");
       break;
     }
-    line[strcspn(line, "\n")] = '\0';
+    line[strcspn(line, "n")] = '0';  // Remove newline character
     if (strcmp(line, "exit") == 0) {
       break;
     } else if (strcmp(line, "env") == 0) {
-      extern char *environ;
-      for (char *env = environ; *env != NULL; env++) {
-        printf("%s\n", *env);
+      extern char * _Nullable * _Nullable environ;
+      for (char * _Nullable *env = environ; *env != NULL; env++) {
+        printf("%sn", *env);
       }
     } else {
       if (execute_command(line) == -1) {
-        printf("%s: command not found\n", line);
+        printf("%s: command not foundn", line);
       }
     }
   }
